d0d0e0839c792606299fa964a89430d46512b9e2
Hongyu Huang <47295471+nicolehhy@users.noreply.github.com>
2019-06-28 15:45:47 -0400

    Update active_uid_front_week_tmp.sh

--- Feed/active_uid_front_week_tmp.sh
+++ Feed/active_uid_front_week_tmp.sh
@@ -1,3 +1,31 @@
+### This part shows the customer behavior in different client( 'Andriod' and 'ios')
+
+### The syntax below involves 'partion' and here is a simple example how to create a partioning-date table. When we partion date 
+### into every year, we can select the data more efficiently. 
+# example for how to partiton the table based on time
+CREATE TABLE part_date3
+    ->      (  c1 int default NULL,
+    ->  c2 varchar(30) default NULL,
+    ->  c3 date default NULL) engine=myisam
+    ->      partition by range (to_days(c3))
+    -> (PARTITION p0 VALUES LESS THAN (to_days('1995-01-01')),
+    -> PARTITION p1 VALUES LESS THAN (to_days('1996-01-01')) ,
+    -> PARTITION p2 VALUES LESS THAN (to_days('1997-01-01')) ,
+    -> PARTITION p3 VALUES LESS THAN (to_days('1998-01-01')) ,
+    -> PARTITION p4 VALUES LESS THAN (to_days('1999-01-01')) ,
+    -> PARTITION p5 VALUES LESS THAN (to_days('2000-01-01')) ,
+    -> PARTITION p6 VALUES LESS THAN (to_days('2001-01-01')) ,
+    -> PARTITION p7 VALUES LESS THAN (to_days('2002-01-01')) ,
+    -> PARTITION p8 VALUES LESS THAN (to_days('2003-01-01')) ,
+    -> PARTITION p9 VALUES LESS THAN (to_days('2004-01-01')) ,
+    -> PARTITION p10 VALUES LESS THAN (to_days('2010-01-01')),
+    -> PARTITION p11 VALUES LESS THAN MAXVALUE );
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+############################################## 
+## Here is the code I used for this project ## 
+############################################## 
+
+### Set The Environment in HIVE ###
 /apps/hive/bin/hive -e "
 set mapreduce.reduce.memory.mb = 8192;
 set mapreduce.map.memory.mb = 8192;
